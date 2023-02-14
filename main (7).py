f6f9fd78df8305896d921ff3df9a318f9e5756c4
sfg11231 <124983641+sfg11231@users.noreply.github.com>
2023-02-14 14:30:43 +0300

    ПР №2 14.02.2023

---
+++ main (7).py
@@ -0,0 +1,16 @@
+a = 2
+b = 5 
+print ((a>b) and (b>a))
+print ((b>a) and (a<b))
+print ((a>b) or (b>a))
+print (not (b>a))
+c = a + b
+print ((c>b) or (b>a))
+print ((a>c) or (b>c))
+print ((c<b) or (b<a))
+print ((a<c) or (b>a))
+f = 'ghbdtn'
+q = 'gjrf'
+print ((f>q) and (q<f))
+print ((q>f) or (q<f))
+print (not(f<q))
