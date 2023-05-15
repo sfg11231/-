4c1071351f4d5d5b7e19d75150d5959ceb023980
sfg11231 <124983641+sfg11231@users.noreply.github.com>
2023-05-15 15:31:32 +0300

    Классная работа 15.05.2023

--- main (30).py
+++ main (30).py
@@ -1,7 +1,29 @@
-a = 1
-while a<=20:
-    b = a + 1
-    c = a**b
-    a = a + 1
-    print(c)
+def schet ():
+    a = int(input ("Введите первое число: "))
+    b = int(input ("Введите второе число: "))
+    c = int(input ("Введите третье число: "))
+    if a>b :
+        print (a-b)
+    elif c>b :
+        print (c-b)
+    else:
+        print (b-a)
+    return a, b, c
+j, k, l = schet()
+o, p, i = schet()
+z, x, c = schet ()
+
+def kuku ():
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    if a>b:
+        print (a-b)
+    else:
+        print(b-a)
+    return a, b 
+a, b = kuku()
+f, g = kuku()
+e, r = kuku()
+
+
     
